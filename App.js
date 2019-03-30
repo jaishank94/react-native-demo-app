5eb2c06581096d2cfb876f10ecb4eb2d7880312d
Jai Shankar Prasad <jai.shankar@funstay.in>
2019-03-30 09:28:33 +0530

    AsyncStored Data

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
 
 import LoginScreen from './screens/LoginScreen';
+import { AsyncStorage } from 'react-native';
 // import HomeScreen from './screens/HomeScreen';
 
 export default class App extends React.Component {
@@ -13,6 +14,53 @@ export default class App extends React.Component {
       otpEntered: false
     }
   }
+
+  componentDidMount() {
+    // AsyncStorage.removeItem('unread_notifications');
+    // AsyncStorage.removeItem('read_notifications');
+    // this.storeData();
+
+  }
+
+  storeData = async () => {
+    let unreadNotify = [
+      {
+        name: 'James Like your post'
+      },
+      {
+        name: 'James commented your post'
+      },
+      {
+        name: 'James gave like post'
+      }
+    ];
+    // You only need to define what will be added or updated
+    let readNotify = [
+      {
+        name: 'Jack Like your post'
+      },
+      {
+        name: 'Sam commented your post'
+      },
+      {
+        name: 'Jenny gave like post'
+      },
+      {
+        name: 'James Like your post'
+      },
+      {
+        name: 'Jacob commented your post'
+      },
+      {
+        name: 'Jerry gave like post'
+      },{
+        name: 'June Like your post'
+      }
+    ];
+
+    AsyncStorage.setItem('unread_notifications', JSON.stringify(unreadNotify));
+    AsyncStorage.setItem('read_notifications', JSON.stringify(readNotify));
+  };
 
   getOtp = () => {
     this.setState({
--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -3,47 +3,102 @@ import {
     View,
     StyleSheet,
     StatusBar,
-    Text
+    Text,
+    ScrollView,
+    AsyncStorage
 } from "react-native";
-import { Card, Button, Icon } from 'react-native-elements'
+import { Card, Button, Icon, colors } from 'react-native-elements';
 
 export default class NotificationScreen extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            unReadNotifications: [],
+            readNotifications: []
+        }
+    }
+
+    componentDidMount() {
+        AsyncStorage.getItem('read_notifications',
+            (err, result) => {
+                this.setState({
+                    readNotifications: result,
+                });
+            });
+        AsyncStorage.getItem('unread_notifications',
+            (err, result) => {
+                this.setState({
+                    unReadNotifications: result,
+                });
+            });
+    }
+
+    renderReadNotifications() {
+        console.log('dads', this.state.readNotifications)
+        return JSON.parse(this.state.readNotifications).map((msg, key) => {
+            return (
+                <Card
+                    key={key}
+                    title={msg.name}
+                    style={{ marginTop: 50 }}
+                >
+                    <Text style={{ marginBottom: 10 }}>
+                        The idea with React Native Elements is more about component structure than actual design.
+                    </Text>
+                </Card>
+            );
+        }
+        )
+    }
+
+    renderUnreadNotifications() {
+        let read = JSON.parse(this.state.readNotifications);
+        let unread = JSON.parse(this.state.unReadNotifications);
+
+        const newArray = unread.concat(read);
+
+        AsyncStorage.removeItem('unread_notifications');
+        AsyncStorage.removeItem('read_notifications');
+
+        AsyncStorage.setItem('read_notifications', JSON.stringify(newArray), () => {
+              AsyncStorage.getItem('read_notifications', (err, result) => {
+                console.log('prod',result);
+              });
+          });
+
+        return JSON.parse(this.state.unReadNotifications).map((msg, key) => {
+            return (
+                <Card
+                    key={key}
+                    title={msg.name}
+                    style={{ marginTop: 50 }}
+                >
+                    <Text style={{ marginBottom: 10, color:'red' }}>
+                        The idea with React Native Elements is more about component structure than actual design.
+                    </Text>
+                </Card>
+            );
+        }
+        )
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <StatusBar barStyle="dark-content" hidden={false} backgroundColor="#00BCD4" translucent={true} />
-                <Card
-                    title='James likes your post'
-                    style={{ marginTop: 50 }}
-                >
-                    <Text style={{ marginBottom: 10 }}>
-                        The idea with React Native Elements is more about component structure than actual design.
-                    </Text>
-                </Card>
-                <Card
-                    title='Sam likes your post'
-                    style={{ marginTop: 50 }}
-                >
-                    <Text style={{ marginBottom: 10 }}>
-                        The idea with React Native Elements is more about component structure than actual design.
-                    </Text>
-                </Card>
-                <Card
-                    title='Jack posted for the first time!'
-                    style={{ marginTop: 50 }}
-                >
-                    <Text style={{ marginBottom: 10 }}>
-                        The idea with React Native Elements is more about component structure than actual design.
-                    </Text>
-                </Card>
-                <Card
-                    title='Jack just joined the team'
-                    style={{ marginTop: 50 }}
-                >
-                    <Text style={{ marginBottom: 10 }}>
-                        The idea with React Native Elements is more about component structure than actual design.
-                    </Text>
-                </Card>
+                <StatusBar barStyle="dark-content" hidden={true} backgroundColor="#00BCD4" translucent={true} />
+                <ScrollView>
+                    {
+                        (this.state.unReadNotifications && this.state.unReadNotifications !== undefined && this.state.unReadNotifications.length > 0) ?
+                            this.renderUnreadNotifications()
+                            : <Text></Text>
+                    }
+                    {
+                        (this.state.readNotifications && this.state.readNotifications !== undefined && this.state.readNotifications.length > 0) ?
+                            this.renderReadNotifications()
+                            : <Text>No New Notifications</Text>
+                    }
+                </ScrollView>
             </View>
         );
     }
